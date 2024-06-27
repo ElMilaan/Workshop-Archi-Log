a596e9d67ff6504157fd8cf1203b86f6e5f4414c
Laurine-sl <laurine.suel@edu.esiee.fr>
2024-06-27 18:17:42 +0200

    modif

--- config/db.js
+++ config/db.js
@@ -1,8 +1,9 @@
 require("dotenv").config({ path: "./config/.env" });
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
   host: process.env.HOST_DB,
+  port: process.env.PORT_DB,
   user: process.env.USER_DB,
   password: process.env.PASSWORD_DB,
   database: process.env.DATABASE_NAME,
